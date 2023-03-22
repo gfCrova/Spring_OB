c0abaec800fbdb5eed60da361df97b8d101c4d1d
gfCrova <giancrova.cl5@gmail>
2023-03-22 00:59:23 -0300

    add test s3

---
+++ Spring_ob-s3_Boot/src/test/java/com/example/Spring_obs3_Boot/SpringObS3BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_obs3_Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObS3BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
