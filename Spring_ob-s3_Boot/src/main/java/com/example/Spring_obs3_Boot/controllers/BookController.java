78ff266a20f13f5f7d22df46ab35905f9e951491
gfCrova <giancrova.cl5@gmail.com>
2023-03-23 00:54:32 -0300

    refactor controller Book

--- Spring_ob-s3_Boot/src/main/java/com/example/Spring_obs3_Boot/controllers/BookController.java
+++ Spring_ob-s3_Boot/src/main/java/com/example/Spring_obs3_Boot/controllers/BookController.java
@@ -1,5 +1,24 @@
 package com.example.Spring_obs3_Boot.controllers;
 
+import com.example.Spring_obs3_Boot.entities.Book;
+import com.example.Spring_obs3_Boot.repositories.BookRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class BookController {
 
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Método para Recuperar datos
+    @GetMapping("/books")
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
 }
